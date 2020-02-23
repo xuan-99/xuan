f6c0280d9c9eddb2e2493faf190c107e22533175
xuan-99 <57170702+xuan-99@users.noreply.github.com>
2020-02-23 11:48:05 +0800

    Add files via upload

---
+++ systemdownload/systemdownload.js
@@ -0,0 +1,20 @@
+//跳转到主站
+
+function zz() {
+    var url = "";
+    window.open(url);
+}
+
+//跳转到注册
+
+function zc() {
+    var url = "";
+    window.open(url);
+}
+
+//下载
+
+function updateBtn() {
+    var url = "";
+    window.open(url);
+}
